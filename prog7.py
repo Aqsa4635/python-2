e3dc640fd99feea59d0f626f9f2a6550141e83ad
Aqsa4635 <aksagowhar123@gmail.com>
2025-12-06 04:46:16 -0800

    Create prog7.py

---
+++ prog7.py
@@ -0,0 +1,9 @@
+a=float(input("enter first number:"))
+b=float(input("enter 2nd number:"))
+c=float(input("enter 3rd number:"))
+if a>b and a>c:
+    print("a is greater")
+elif b>c and b>a:
+    print("b is greater")
+else:
+    print("c is greater")
