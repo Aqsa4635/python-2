54cd5e572ead16223d22b2d9d04531bf4aac8e12
Aqsa4635 <aksagowhar123@gmail.com>
2025-12-05 22:54:15 -0800

    Concatenate.py

---
+++ strlen.py
@@ -0,0 +1,5 @@
+str1_= input("enter first string:")
+str2_= input("enter second string:")
+Concatenate= str1_ + str2_
+print("concatenated string:", Concatenate)
+print("length of concatenate:", len(Concatenate))
